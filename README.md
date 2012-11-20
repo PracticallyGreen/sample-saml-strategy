5abcd68965b888c5d9644c679e65a41e774d7caa
Rajiv Aaron Manglani <rajiv@practicallygreen.com>
2012-11-20 13:14:02 -0500

    update to omniauth-saml 1.0.0.
    * add README and additional debugging.

---
+++ README.md
@@ -0,0 +1,11 @@
+# Sample SAML Strategy
+
+A sample Rails application for testing the OmniAuth SAML gem.
+
+See https://github.com/PracticallyGreen/omniauth-saml for setup instructions.
+
+## Authors
+
+Authored by Raecoo Cao and Rajiv Aaron Manglani.
+
+Maintained by [Rajiv Aaron Manglani](http://www.rajivmanglani.com/).
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,15 @@
 class SessionsController < ApplicationController
-  
+
   def create
-    flash[:notice] = "Login Successfully" 
+    flash[:notice] = "Login Successfully"
     @provider = request.env['omniauth.auth']['provider']
     @uid      = request.env['omniauth.auth']['uid']
+
+    Rails.logger.info request.env['omniauth.auth'].to_hash.inspect
   end
-  
+
   def failure
     flash[:notice] = "Auth failure."
   end
-  
+
 end
